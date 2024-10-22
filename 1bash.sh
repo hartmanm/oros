f78ec64482c64a67f9b0d0f27cff1a996c27aa0d
Michael Neill Hartman <16545449+hartmanm@users.noreply.github.com>
2024-10-22 09:40:14 -0700

    added description

--- 1bash.sh
+++ 1bash.sh
@@ -3,6 +3,7 @@
 # Copyright (c) 2018 Michael Neill Hartman. All rights reserved.
 # mnh_license@proton.me
 # https://github.com/hartmanm
+# os side scripts for oros / overlord (previously openrig.net)
 
 BASH_VERSION="1.5"
 SUB_VERSION="1"
